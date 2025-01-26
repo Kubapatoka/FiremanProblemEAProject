2fb654fa10316363c5cf7a2aeee89176d35b6bb4
Kubapatoka <bukaj.k@wp.pl>
2025-01-26 18:50:54 +0100

    effective and useless firefighters

--- ProblemDef.py
+++ ProblemDef.py
@@ -56,6 +56,40 @@ class FirefighterProblem:
         burned_number -= len(self.fire_starts)
 
         return burned_number
+    
+    def effective_and_useless_firefighters_count(self, fireman):
+        number_of_nodes = self.graph.number_of_nodes()
+        burned = np.repeat(False, number_of_nodes)
+
+        queue = []
+        for f in self.fire_starts:
+            queue.append(f)
+            burned[f] = True
+
+        while not queue:
+            first_elem = queue.pop(0)
+            neighbours = self.graph.neighbors(first_elem)
+            for n in neighbours:
+                if n in queue or burned[n] or n in fireman:
+                    continue
+                queue.append(n)
+
+        effective_count = 0
+        useless_count = 0
+
+        for f in fireman:
+            burn = False
+            not_burn = False
+
+            for n in self.graph.neighbors(f):
+                if n in fireman: continue
+                if burned[n]: burn = True
+                else: not_burn = True
+            
+            if burn and not_burn: effective_count +=1
+            if not_burn and not burn: useless_count += 1
+        return (effective_count, useless_count)
+
 
 
 @classmethod
