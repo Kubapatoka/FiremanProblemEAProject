9c127d27845f207981e4c5674aa30ed3f9fdea8f
Kubapatoka <bukaj.k@wp.pl>
2025-01-27 21:33:50 +0100

    missing file

--- SGA/SGAInstance.py
+++ SGA/SGAInstance.py
@@ -22,42 +22,43 @@ class SGAInstance:
         rv = []
         for c in candidates:
             if np.random.random() < self._mutationProb:
-                rv.append(self._fixer(self._mutator(c)))
+                rv.append(self._fixer(self._mutator(c, self.problem)))
             else:
                 rv.append(c)
         return rv
-    
-    
+
+
     def Crossover(self, parents: tuple[list[bool], list[bool]]):
         cand = self._crossover(parents[0], parents[1])
         return tuple[self._fixer(cand[0]),self._fixer(cand[1])]
-    
-    
+
+
     def Replacement(self, population: list[tuple[list[bool],int]], candidates: list[list[bool]]):
         new_population = population
         for c in candidates:
-            new_population.append(tuple[c, self._evaluator(c)])
-        
+            new_population.append(tuple[c, self._evaluator(c, self.problem)])
+
         new_population.sort(key=lambda v: v[1])
         return new_population[:self._populationSize]
-    
+
+
     def ParentSelection(self, population: list[list[bool],int]):
         self._currentIteration += 1
-        
+
         idxs : tuple[int,int] = self._parentsSelector(population)
         return tuple[population[idxs[0]][0],population[idxs[1]][0]]
 
-    
+
     def TerminationCondition(self):
         if self._currentIteration >= self._generationsNumber:
             return True
         return False
 
-    
-    def InitialPopulation(self):
-        return self._populationInitializer(self._populationSize, self._chromosomeLength)
 
-    
+    def InitialPopulation(self):
+        return self._populationInitializer(self._populationSize, self._chromosomeLength, self.problem, self._evaluator)
+
+
     def bestIndividual(self, population: list[tuple[list[bool],int]]):
         if self.best_solution == None or self.best_solution[1] > population[0][1]:
             self.best_solution = population[0]
