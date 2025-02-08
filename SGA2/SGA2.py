c0134ffd11df7d5fac5074a5fb7e1fbcf6f5a8da
Kubapatoka <bukaj.k@wp.pl>
2025-02-08 07:24:50 +0100

    Clean up and start sga2

--- SGA2/SGA2.py
+++ SGA2/SGA2.py
@@ -3,7 +3,7 @@ import ProblemDef
 def SGA(instance):
     population = instance.InitialPopulation()
     # population is a sorted list of pairs: (genotype, fitness_value)
-    
+
     while not instance.TerminationCondition():
         parents = instance.ParentSelection(population)
         # candidates are genotype
