8b6fc5265476fabdcf71e51e4c5e7c661edc7e3e
Szymon Jedras <szymon.jedras.wr@gmail.com>
2025-02-09 20:30:49 +0100

    Evaluator for SGA2

--- Evaluators.py
+++ Evaluators.py
@@ -1,24 +1,24 @@
 from ProblemDef import FirefighterProblem
+from ProblemDef2 import IncrementalFirefighterProblem
 import networkx as nx
 import numpy as np
 
 
 class BaseEvaluator:
-    def propagate_fire(self, problem, on_fire, burnt, firefighters):
+    def propagate_fire(self, problem, on_fire, burnt):
         neighbors = set.union(*(set(problem.graph.neighbors(v)) for v in on_fire))
         burnt |= on_fire
-        return neighbors - burnt - firefighters
+        return neighbors - burnt
 
 
 class MainEvaluator(BaseEvaluator):
     def __call__(self, problem: FirefighterProblem, o):
-        burnt = set()
+        burnt = set(np.flatnonzero(o))
         on_fire = set(problem.fire_starts)
-        firefighters = set(np.flatnonzero(o))
-        default = set(problem.graph.nodes) - on_fire - firefighters
+        default = set(problem.graph.nodes) - on_fire - burnt
 
         while on_fire:
-            on_fire = self.propagate_fire(problem, on_fire, burnt, firefighters)
+            on_fire = self.propagate_fire(problem, on_fire, burnt)
             default -= on_fire
 
         return len(default)
@@ -26,16 +26,52 @@ class MainEvaluator(BaseEvaluator):
 
 class CummulativeEvaluator(BaseEvaluator):
     def __call__(self, problem: FirefighterProblem, o):
-        burnt = set()
+        burnt = set(np.flatnonzero(o))
         on_fire = set(problem.fire_starts)
-        firefighters = set(np.flatnonzero(o))
-        default = set(problem.graph.nodes) - on_fire - firefighters
+        default = set(problem.graph.nodes) - on_fire - burnt
         value = 0
 
         for _ in range(len(problem.graph.nodes)):
             if on_fire:
-                on_fire = self.propagate_fire(problem, on_fire, burnt, firefighters)
+                on_fire = self.propagate_fire(problem, on_fire, burnt)
                 default -= on_fire
             value += len(default)
 
         return value
+
+class BaseIncrementalEvaluator(BaseEvaluator):
+    def pick_k(self, perm, k, unavailable):
+        valid = [i for i in range(len(perm)) if perm[i] not in unavailable]
+        return perm[valid[:k]]
+
+class IncrementalMainEvaluator(BaseIncrementalEvaluator):
+    def __call__(self, problem: IncrementalFirefighterProblem, fireman):
+        on_fire = set(problem.fire_starts)
+        unavailable = set(self.pick_k(fireman, problem.num_teams_start, on_fire)) | on_fire
+        default = set(problem.graph.nodes) - unavailable
+
+        while on_fire:
+            new_firefighters = set(self.pick_k(fireman, problem.num_teams_increment, unavailable))
+            unavailable |= new_firefighters
+            on_fire = self.propagate_fire(problem, on_fire, unavailable)
+            default = default - on_fire - new_firefighters
+
+        return len(default)
+
+class IncrementalCummulativeEvaluator(BaseIncrementalEvaluator):
+    def __call__(self, problem: IncrementalFirefighterProblem, fireman):
+        on_fire = set(problem.fire_starts)
+        unavailable = set(self.pick_k(fireman, problem.num_teams_start, on_fire)) | on_fire
+        default = set(problem.graph.nodes) - unavailable
+        value = 0
+
+        for _ in range(len(problem.graph)):
+            if on_fire:
+                new_firefighters = set(self.pick_k(fireman, problem.num_teams_increment, unavailable))
+                unavailable |= new_firefighters
+                on_fire = self.propagate_fire(problem, on_fire, unavailable)
+                default = default - on_fire - new_firefighters
+            value += len(default)
+
+        return value
+
